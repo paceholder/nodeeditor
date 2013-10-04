4c62fe19a58db58260da7d30a526ddf44c3620be
Dmitry Pinaev <prof-x@inbox.ru>
2013-10-04 22:07:31 +0200

    Completet FlowItemInterface

--- FlowItemInterface.hpp
+++ FlowItemInterface.hpp
@@ -4,17 +4,35 @@
 class FlowItemInterface
 {
 public:
+  /** Returns the type of curren flow item */
+  virtual
+  QString
+  getFlowItemSignature() const = 0;
+
+  /** Caption of the block  */
   virtual
   QString
   getNodeTitle() const = 0;
 
+  /** Signatures (types) of input node connections */
   virtual
   QStringList
   getNodeInSignatures() const = 0;
 
+  /** Names (text to be printed) of input node connections */
+  virtual
+  QStringList
+  getNodeInNames() const = 0;
+
+  /** Signatures (types) of output node connections */
   virtual
   QStringList
   getNodeOutSignatures() const = 0;
+
+  /** Names (text to be printed) of output node connections */
+  virtual
+  QStringList
+  getNodeOutNames() const = 0;
 
   virtual
   QMap<id, QVariant>
@@ -23,5 +41,11 @@ public:
   virtual
   QVariant
   getData() const = 0;
+
+public:
+  /** clone constructor */
+  virtual
+  FlowItemInterface*
+  clone() const = 0;
 };
 #endif // FLOW_ITEM_INTERFACE
