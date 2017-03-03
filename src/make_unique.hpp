482ade282be95e73cb5fdef476460fb7fbdd8cb1
Davide Faconti <davide.faconti@gmail.com>
2017-03-03 18:24:23 +0100

    downgrade requirements to c++11 and Qt 5.2

---
+++ src/make_unique.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <memory>
+
+#if __cplusplus < 201300 //c++14
+namespace std
+{
+    template<typename T, typename... Args>
+    std::unique_ptr<T> make_unique(Args&&... args)
+    {
+        return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+    }
+}
+
+#endif
