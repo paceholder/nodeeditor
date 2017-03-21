9d6221e07bc7a4961dc92b6c756ec83da16d4268
Russell Greene <russellgreene8@gmail.com>
2017-03-20 19:39:03 -0600

    Fix constructors actually

--- src/FlowView.cpp
+++ src/FlowView.cpp
@@ -48,14 +48,12 @@ FlowView(FlowScene *scene)
   //setViewport(new QGLWidget(QGLFormat(QGL::SampleBuffers)));
 
   // setup actions
-  _clearSelectionAction = new QAction;
-  _clearSelectionAction->setText(QStringLiteral("Clear Selection"));
+  _clearSelectionAction = new QAction(QStringLiteral("Clear Selection"), this);
   _clearSelectionAction->setShortcut(Qt::Key_Escape);
   connect(_clearSelectionAction, &QAction::triggered, _scene, &QGraphicsScene::clearSelection);
   addAction(_clearSelectionAction);
 
-  _deleteSelectionAction = new QAction;
-  _deleteSelectionAction->setText(QStringLiteral("Delete Selection"));
+  _deleteSelectionAction = new QAction(QStringLiteral("Delete Selection"), this);
   _deleteSelectionAction->setShortcut(Qt::Key_Delete);
   connect(_deleteSelectionAction, &QAction::triggered, this, &FlowView::deleteSelectedNodes);
   addAction(_deleteSelectionAction);
