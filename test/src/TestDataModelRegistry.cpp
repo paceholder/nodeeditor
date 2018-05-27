db51d9025343e8fc12f29de6a796c54454510c98
Justin Bassett <jbassett271@gmail.com>
2018-05-27 15:00:16 -0700

    Implement automated tests (#177)
    * add basic test support
    
    * fix travis.yml
    
    * fix-travis-osx
    
    * fix .appveyor.yml
    
    * Added some dragging tests
    
    * remove failing test
    
    * Improve naming, reuse of code
    
    * fix bug

---
+++ test/src/TestDataModelRegistry.cpp
@@ -0,0 +1,45 @@
+#include <nodes/DataModelRegistry>
+
+#include <catch.hpp>
+
+#include "StubNodeDataModel.hpp"
+
+using QtNodes::DataModelRegistry;
+using QtNodes::NodeData;
+using QtNodes::NodeDataModel;
+using QtNodes::NodeDataType;
+using QtNodes::PortIndex;
+using QtNodes::PortType;
+
+namespace
+{
+class StubModelStaticName : public StubNodeDataModel
+{
+public:
+  static QString
+  Name()
+  {
+    return "Name";
+  }
+};
+}
+
+TEST_CASE("DataModelRegistry::registerModel", "[interface]")
+{
+  DataModelRegistry registry;
+
+  SECTION("stub model")
+  {
+    registry.registerModel<StubNodeDataModel>();
+    auto model = registry.create("name");
+
+    CHECK(model->name() == "name");
+  }
+  SECTION("stub model with static name")
+  {
+    registry.registerModel<StubModelStaticName>();
+    auto model = registry.create("Name");
+
+    CHECK(model->name() == "name");
+  }
+}
