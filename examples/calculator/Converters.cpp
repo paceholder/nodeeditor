edaaeac4594d7374c7403469c1a8bf25e691a0af
Dmitry Pinaev <dimitry.pinaev@gmail.com>
2019-03-18 12:58:04 +0100

    Fix data propagation in converter (in calculator example) (#217)

--- examples/calculator/Converters.cpp
+++ examples/calculator/Converters.cpp
@@ -6,7 +6,6 @@
 #include "IntegerData.hpp"
 
 
-
 std::shared_ptr<NodeData>
 DecimalToIntegerConverter::
 operator()(std::shared_ptr<NodeData> data)
@@ -17,6 +16,10 @@ operator()(std::shared_ptr<NodeData> data)
   if (numberData)
   {
     _integer = std::make_shared<IntegerData>(numberData->number());
+  }
+  else
+  {
+    _integer.reset();
   }
 
   return _integer;
@@ -34,7 +37,10 @@ operator()(std::shared_ptr<NodeData> data)
   {
     _decimal = std::make_shared<DecimalData>(numberData->number());
   }
+  else
+  {
+    _decimal.reset();
+  }
 
   return _decimal;
 }
-
