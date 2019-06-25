6d32901d1adfbdfda5f9c8ddb2c34e49b5516f43
Malcolm Tyrrell <Malcolm.R.Tyrrell@gmail.com>
2019-06-25 21:18:35 +0100

    Make deleteConnection take const.

--- include/nodes/internal/FlowScene.hpp
+++ include/nodes/internal/FlowScene.hpp
@@ -54,7 +54,7 @@ public:
 
   std::shared_ptr<Connection> restoreConnection(QJsonObject const &connectionJson);
 
-  void deleteConnection(Connection& connection);
+  void deleteConnection(const Connection& connection);
 
   Node&createNode(std::unique_ptr<NodeDataModel> && dataModel);
 
