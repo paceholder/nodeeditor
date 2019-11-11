bf13b93f4e24b110fc64ccf6ce271d73df449e22
Taiguara <tatatupi@gmail.com>
2019-11-11 17:19:35 -0300

    clearing scene only if file open doesn't fail

--- src/FlowScene.cpp
+++ src/FlowScene.cpp
@@ -500,10 +500,6 @@ void
 FlowScene::
 load()
 {
-  clearScene();
-
-  //-------------
-
   QString fileName =
     QFileDialog::getOpenFileName(nullptr,
                                  tr("Open Flow Scene"),
@@ -517,6 +513,8 @@ load()
 
   if (!file.open(QIODevice::ReadOnly))
     return;
+
+  clearScene();
 
   QByteArray wholeFile = file.readAll();
 
