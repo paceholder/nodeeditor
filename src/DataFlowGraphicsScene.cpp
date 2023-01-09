paceholder/nodeeditor

commit 883e8a003e4b84e00931f62a284b99a7c7f13d06

Fix a bug in the scene menu search, update the treeView creation process

src/DataFlowGraphicsScene.cpp
@@ -76,8 +76,6 @@ createSceneMenu(QPointF const scenePos)
 {
   QMenu* modelMenu = new QMenu();
 
-  auto skipText = QStringLiteral("skip me");
-
   // Add filterbox to the context menu
   auto* txtBox = new QLineEdit(modelMenu);
   txtBox->setPlaceholderText(QStringLiteral("Filter"));
@@ -101,21 +99,23 @@ createSceneMenu(QPointF const scenePos)
 
   auto registry = _graphModel.dataModelRegistry();
 
-  QMap<QString, QTreeWidgetItem*> topLevelItems;
   for (auto const& cat : registry->categories())
   {
     auto item = new QTreeWidgetItem(treeView);
     item->setText(0, cat);
-    item->setData(0, Qt::UserRole, skipText);
-    topLevelItems[cat] = item;
+    item->setFlags(item->flags() & ~Qt::ItemIsSelectable);
   }
 
   for (auto const& assoc : registry->registeredModelsCategoryAssociation())
   {
-    auto parent = topLevelItems[assoc.second];
-    auto item = new QTreeWidgetItem(parent);
+    QList<QTreeWidgetItem*> parent = treeView->findItems(assoc.second, 
+                                                         Qt::MatchExactly);
+
+    if (parent.count() <= 0)
+      continue;
+
+    auto item = new QTreeWidgetItem(parent.first());
     item->setText(0, assoc.first);
-    item->setData(0, Qt::UserRole, assoc.first);
   }
 
   treeView->expandAll();
@@ -123,17 +123,14 @@ createSceneMenu(QPointF const scenePos)
   connect(treeView, &QTreeWidget::itemClicked,
           [this,
            modelMenu,
-           skipText,
            scenePos](QTreeWidgetItem* item, int)
           {
-            QString modelName = item->data(0, Qt::UserRole).toString();
-
-            if (modelName == skipText)
+            if(!(item->flags() & (Qt::ItemIsSelectable)))
             {
               return;
             }
 
-            NodeId nodeId = this->_graphModel.addNode(modelName);
+            NodeId nodeId = this->_graphModel.addNode(item->text(0));
 
             if (nodeId != InvalidNodeId)
             {
@@ -147,17 +144,16 @@ createSceneMenu(QPointF const scenePos)
 
   //Setup filtering
   connect(txtBox, &QLineEdit::textChanged,
-          [&](const QString& text)
+          [treeView](const QString& text)
           {
-            for (auto& topLvlItem : topLevelItems)
+            QTreeWidgetItemIterator it(treeView, QTreeWidgetItemIterator::NoChildren);
+            while (*it)
             {
-              for (int i = 0; i < topLvlItem->childCount(); ++i)
-              {
-                auto child = topLvlItem->child(i);
-                auto modelName = child->data(0, Qt::UserRole).toString();
-                const bool match = (modelName.contains(text, Qt::CaseInsensitive));
-                child->setHidden(!match);
-              }
+              auto modelName = (*it)->data(0, Qt::UserRole).toString();
+              const bool match = (modelName.contains(text, Qt::CaseInsensitive));
+              (*it)->setHidden(!match);
+
+              ++it;
             }
           });
 

commit ce3230bbd1e2d68a4307366b7cff9898cea8def0

Fixed broken context search and hide empty categories from filter.
Searching through the context menu was broken after 883e8a0 due to a
mis-comparison on node data that is never inserted. This has been fixed
by correctly comparing QTreeWidgetItem::text.

Furthermore, the filtering system has been changed to hide empty
categories, showing only those that are relevant (contain matching
nodes).

Signed-off-by: Finn Rayment <finn@rayment.fr>

src/DataFlowGraphicsScene.cpp
@@ -140,13 +140,24 @@ createSceneMenu(QPointF const scenePos)
   connect(txtBox, &QLineEdit::textChanged,
           [treeView](const QString& text)
           {
+            QTreeWidgetItemIterator categoryIt(treeView, QTreeWidgetItemIterator::HasChildren);
+            while (*categoryIt)
+              (*categoryIt++)->setHidden(true);
             QTreeWidgetItemIterator it(treeView, QTreeWidgetItemIterator::NoChildren);
             while (*it)
             {
-              auto modelName = (*it)->data(0, Qt::UserRole).toString();
+              auto modelName = (*it)->text(0);
               const bool match = (modelName.contains(text, Qt::CaseInsensitive));
               (*it)->setHidden(!match);
-
+              if (match)
+              {
+                QTreeWidgetItem *parent = (*it)->parent();
+                while (parent)
+                {
+                  parent->setHidden(false);
+                  parent = parent->parent();
+                }
+              }
               ++it;
             }
           });