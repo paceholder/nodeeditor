41957ea2b04101d98a9485174913e1b40708e9ee
Finn Rayment <finn@rayment.fr>
2023-01-28 22:24:35 +1100

    Fixed unused variable.

--- include/QtNodes/internal/DataFlowGraphModel.hpp
+++ include/QtNodes/internal/DataFlowGraphModel.hpp
@@ -98,10 +98,7 @@ public Q_SLOTS:
     virtual void propagate(NodeId const nodeId);
 
 protected:
-    virtual bool canPropagate(ConnectionId const connectionId)
-    {
-        return true;
-    }
+    virtual bool canPropagate(ConnectionId const) { return true; }
 
 Q_SIGNALS:
     void inPortDataWasSet(NodeId const, PortType const, PortIndex const);
