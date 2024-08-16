43a39bc3c2d727c9ee1e4555e0bd13dd42959275
Dmitry Pinaev <dimitry.pinaev@gmail.com>
2024-08-16 21:53:55 +0200

    Update and rename ConnectionPainter.hpp to DefaultConnectionPainter.hpp

--- src/DefaultConnectionPainter.hpp
+++ src/DefaultConnectionPainter.hpp
@@ -11,17 +11,11 @@ namespace QtNodes {
 class ConnectionGeometry;
 class ConnectionGraphicsObject;
 
-class ConnectionPainter : public AbstractConnectionPainter
+class DefaultConnectionPainter : public AbstractConnectionPainter
 {
 public:
     void paint(QPainter *painter, ConnectionGraphicsObject const &cgo) const override;
     QPainterPath getPainterStroke(ConnectionGraphicsObject const &cgo) const override;
-
-private:
-    static QPainterPath cubicPath(ConnectionGraphicsObject const &connection);
-    static void drawSketchLine(QPainter *painter, ConnectionGraphicsObject const &cgo);
-    static void drawHoveredOrSelected(QPainter *painter, ConnectionGraphicsObject const &cgo);
-    static void drawNormalLine(QPainter *painter, ConnectionGraphicsObject const &cgo);
 };
 
 } // namespace QtNodes
