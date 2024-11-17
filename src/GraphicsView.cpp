34b6f81031ad75e64313bf14048bb3dc782c7894
Dmitry Pinaev <dimitry.pinaev@gmail.com>
2024-11-17 15:33:46 +0100

    Do not auto repeat scene actions (#426)

--- src/GraphicsView.cpp
+++ src/GraphicsView.cpp
@@ -93,6 +93,7 @@ void GraphicsView::setScene(BasicGraphicsScene *scene)
         _deleteSelectionAction = new QAction(QStringLiteral("Delete Selection"), this);
         _deleteSelectionAction->setShortcutContext(Qt::ShortcutContext::WidgetShortcut);
         _deleteSelectionAction->setShortcut(QKeySequence(QKeySequence::Delete));
+        _deleteSelectionAction->setAutoRepeat(false);
         connect(_deleteSelectionAction,
                 &QAction::triggered,
                 this,
@@ -106,6 +107,7 @@ void GraphicsView::setScene(BasicGraphicsScene *scene)
         _duplicateSelectionAction = new QAction(QStringLiteral("Duplicate Selection"), this);
         _duplicateSelectionAction->setShortcutContext(Qt::ShortcutContext::WidgetShortcut);
         _duplicateSelectionAction->setShortcut(QKeySequence(Qt::CTRL | Qt::Key_D));
+        _duplicateSelectionAction->setAutoRepeat(false);
         connect(_duplicateSelectionAction,
                 &QAction::triggered,
                 this,
@@ -119,6 +121,7 @@ void GraphicsView::setScene(BasicGraphicsScene *scene)
         _copySelectionAction = new QAction(QStringLiteral("Copy Selection"), this);
         _copySelectionAction->setShortcutContext(Qt::ShortcutContext::WidgetShortcut);
         _copySelectionAction->setShortcut(QKeySequence(QKeySequence::Copy));
+        _copySelectionAction->setAutoRepeat(false);
         connect(_copySelectionAction,
                 &QAction::triggered,
                 this,
@@ -132,6 +135,7 @@ void GraphicsView::setScene(BasicGraphicsScene *scene)
         _pasteAction = new QAction(QStringLiteral("Copy Selection"), this);
         _pasteAction->setShortcutContext(Qt::ShortcutContext::WidgetShortcut);
         _pasteAction->setShortcut(QKeySequence(QKeySequence::Paste));
+        _pasteAction->setAutoRepeat(false);
         connect(_pasteAction, &QAction::triggered, this, &GraphicsView::onPasteObjects);
 
         addAction(_pasteAction);
