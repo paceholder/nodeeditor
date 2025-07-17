f2c120f1a0a82fbb0b7da8028ce19007dfb57966
g-abilio <gabrielabiliobferreira2@gmail.com>
2025-07-17 15:35:48 -0300

    clean up test code in multiplication node

--- examples/calculator/MultiplicationModel.hpp
+++ examples/calculator/MultiplicationModel.hpp
@@ -14,18 +14,6 @@
 class MultiplicationModel : public MathOperationDataModel
 {
 public:
-    MultiplicationModel() {
-        /*
-        QObject::connect(this, &NodeDelegateModel::computingStarted, this, [this]() {
-            this->setNodeProcessingStatus(
-                NodeDelegateModel::NodeProcessingStatus::Status::Processing);
-        });
-        QObject::connect(this, &NodeDelegateModel::computingFinished, this, [this]() {
-            this->setNodeProcessingStatus(
-                NodeDelegateModel::NodeProcessingStatus::Status::Updated);
-        });
-*/
-    }
     virtual ~MultiplicationModel() {}
 
 public:
@@ -41,27 +29,16 @@ private:
         auto n1 = _number1.lock();
         auto n2 = _number2.lock();
 
-        // Dentro da sua classe:
-        QTimer *timer = new QTimer(this);
-        timer->start(1000); // 1000ms = 1 segundo
-        int secondsRemaining = 3;
-        connect(timer, &QTimer::timeout, this, [=]() mutable {
-            if (--secondsRemaining <= 0) {
-                timer->stop();
-                if (n1 && n2) {
-                    //modelValidationState = NodeValidationState::Valid;
-                    //modelValidationError = QString();
-                    _result = std::make_shared<DecimalData>(n1->number() * n2->number());
-                } else {
-                    //modelValidationState = NodeValidationState::Warning;
-                    //modelValidationError = QStringLiteral("Missing or incorrect inputs");
-                    _result.reset();
-                }
+        if (n1 && n2) {
+            //modelValidationState = NodeValidationState::Valid;
+            //modelValidationError = QString();
+            _result = std::make_shared<DecimalData>(n1->number() * n2->number());
+        } else {
+            //modelValidationState = NodeValidationState::Warning;
+            //modelValidationError = QStringLiteral("Missing or incorrect inputs");
+            _result.reset();
+        }
 
-                Q_EMIT dataUpdated(outPortIndex);
-                // Faça algo quando o timer acabar
-            }
-            // Atualize sua UI aqui, ex: label->setText(QString::number(secondsRemaining));
-        });
+        Q_EMIT dataUpdated(outPortIndex);
     }
 };
