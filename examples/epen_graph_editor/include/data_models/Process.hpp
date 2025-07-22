84fc283819d536782a8cbc8af18bd3319148afef
mahmoodzaker <mahmoodzaker@gmail.com>
2025-07-22 17:29:19 +0330

    Grid/Block size to process node

--- examples/epen_graph_editor/include/data_models/Process.hpp
+++ examples/epen_graph_editor/include/data_models/Process.hpp
@@ -7,6 +7,14 @@ class Process : public OperationDataModel
 public:
     virtual ~Process() {}
 
+    Q_PROPERTY(QString Grid_Size_X MEMBER _gridX NOTIFY propertyChanged)
+    Q_PROPERTY(QString Grid_Size_Y MEMBER _gridY NOTIFY propertyChanged)
+    Q_PROPERTY(QString Grid_Size_Z MEMBER _gridZ NOTIFY propertyChanged)
+
+    Q_PROPERTY(QString Block_Size_X MEMBER _blockX NOTIFY propertyChanged)
+    Q_PROPERTY(QString Block_Size_Y MEMBER _blockY NOTIFY propertyChanged)
+    Q_PROPERTY(QString Block_Size_Z MEMBER _blockZ NOTIFY propertyChanged)
+
     QString caption() const override { return QStringLiteral("Process"); }
 
     bool portCaptionVisible(PortType portType, PortIndex portIndex) const override
@@ -48,11 +56,19 @@ public:
 
     void removePortType(PortIndex portIndex)
     {
-        if (portIndex  < nodeTypeMap.size()) {
-            nodeTypeMap.removeAt(portIndex );
+        if (portIndex < nodeTypeMap.size()) {
+            nodeTypeMap.removeAt(portIndex);
         }
     }
 
 private:
     QVector<bool> nodeTypeMap{true};
+
+    QString _gridX;
+    QString _gridY;
+    QString _gridZ;
+
+    QString _blockX;
+    QString _blockY;
+    QString _blockZ;
 };
