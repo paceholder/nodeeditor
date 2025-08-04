026bafd4dddab45064c6ce61c38f7ba76b9df16e
mahmoodzaker <mahmoodzaker@gmail.com>
2025-08-05 02:59:47 +0330

    Seperating lexer and code editor classes

--- examples/epen_graph_editor/include/panels/FloatingCodeEditor.hpp
+++ examples/epen_graph_editor/include/panels/FloatingCodeEditor.hpp
@@ -3,10 +3,8 @@
 
 #include "FloatingPanelBase.hpp"
 #include <QTimer>
-#include <QSet>
 #include <memory>
 #include <map>
-#include <Qsci/qscilexercustom.h>
 
 class QComboBox;
 class QCheckBox;
@@ -15,59 +13,7 @@ class QsciScintilla;
 class CompileResultsWidget;
 class SimpleGPUCompiler;
 class GraphEditorWindow;
-
-// Custom lexer for GPU languages
-class GPULanguageLexer : public QsciLexerCustom
-{
-    Q_OBJECT
-
-public:
-    enum {
-        Default = 0,
-        Comment = 1,
-        CommentLine = 2,
-        Number = 3,
-        Keyword = 4,
-        LanguageKeyword = 5,
-        String = 6,
-        Operator = 7,
-        Identifier = 8,
-        Preprocessor = 9
-    };
-
-    explicit GPULanguageLexer(QObject *parent = nullptr);
-
-    const char *language() const override;
-    QString description(int style) const override;
-    QColor defaultColor(int style) const override;
-    QColor defaultPaper(int style) const override;
-    QFont defaultFont(int style) const override;
-    void styleText(int start, int end) override;
-
-    void setLanguageMode(const QString &language);
-    void setDarkMode(bool dark);
-    
-    // Get collected identifiers
-    QSet<QString> getIdentifiers() const { return m_identifiers; }
-    void clearIdentifiers() { m_identifiers.clear(); }
-    
-    // Public helper methods for syntax checking
-    bool isKeyword(const QString &word) const;
-    bool isLanguageKeyword(const QString &word) const;
-    bool isBuiltinFunction(const QString &word) const;
-    bool isBuiltinType(const QString &word) const;
-
-private:
-    bool isNumber(const QString &text) const;
-
-    QString m_language;
-    bool m_isDarkMode;
-    QSet<QString> m_keywords;
-    QSet<QString> m_languageKeywords;
-    QSet<QString> m_builtinFunctions;
-    QSet<QString> m_types;
-    QSet<QString> m_identifiers; // Track identifiers found during lexing
-};
+class GPULanguageLexer;
 
 class FloatingCodeEditor : public FloatingPanelBase
 {
